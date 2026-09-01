9b663e425578cfbaf58098b14f8e7774ecba0542
rebeccavicente <rvallezicunhaesilva@byupathway.edu>
2026-09-01 11:11:06 -0300

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
-    }
+                
+        string answer="yes";
+
+
+        while (answer=="yes")
+        {
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 11);
+            Console.WriteLine("What is the magic number?");
+            string userInput=Console.ReadLine();
+            int guess = int.Parse(userInput);
+            int quantityOfGuesses=1;
+            while (guess!=magicNumber)
+            {
+                
+                quantityOfGuesses=quantityOfGuesses+1;
+                if (guess>magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                    userInput=Console.ReadLine();
+                    guess = int.Parse(userInput);
+                }
+                else if (guess<magicNumber)
+                    {
+                        Console.WriteLine("Higher");
+                        userInput=Console.ReadLine();
+                        guess = int.Parse(userInput);
+                    }
+                else
+                {
+                    Console.WriteLine("No");
+                }
+            }
+
+            if (guess==magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+            
+                }
+
+            Console.WriteLine($"It took you {quantityOfGuesses} to guess it.");
+            Console.WriteLine("Do you want to play again?");
+            answer=Console.ReadLine();
+
+        }
+
+
+    } 
 }
