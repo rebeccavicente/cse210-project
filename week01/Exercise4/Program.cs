b655f00401e270ef03e43502cd80d5a7e9198bd8
rebeccavicente <rvallezicunhaesilva@byupathway.edu>
2026-09-01 13:33:17 -0300

    exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,51 @@
 using System;
+using System.Numerics;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        int number=1;
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Please enter as many numbers as you can think of. Press 0 when you are done");
+        int maxNumber=0;
+        int count=0;
+
+        while (number!=0)
+        {
+            
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+                {
+                    numbers.Add(number);
+                    count=count+1;
+                }
+        }
+
+        foreach (int item in numbers)
+        {
+                    
+                 if (item>maxNumber)
+                {
+                maxNumber=item;
+                
+                }
+        }
+         Console.WriteLine($"The largest number is {maxNumber}");
+
+        int total = 0;
+
+        foreach (int item in numbers)
+        {
+            total += item;
+        }
+
+        Console.WriteLine($"The total is {total}");
+        int average=total/count;
+        Console.WriteLine($"The average is {average}");
+
+
     }
+
+    
 }
